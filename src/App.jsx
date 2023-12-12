fbfb7f3d6cb84cdb907db1712d537b1566cedee9
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2023-12-12 17:10:19 -0500

    set up the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
+import ContactList from './components/ContactList'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
+        <ContactList />
     </>
   )
 }
